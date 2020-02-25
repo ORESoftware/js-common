744b826cb87e491f56d115d6ab762b2c0a6ef78a
Operations Research Engineering Software+ <alex@oresoftware.com>
2020-02-24 23:26:18 -0800

    tmp

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,17 @@
+import {cp_tables} from "./read-only/types";
+
+type CPUsersAuth = cp_tables.cp_users;
+
+type CPUsersId = cp_tables.cp_users_fields.id;
 
 
-// import {cp_tables} from "../es";
+const v: CPUsersAuth = {
+  age: 0,
+  api_key: "",
+  created: undefined,
+  email: "",
+  id: "",
+  language: "",
+  name: "",
+  verified: false
+};
